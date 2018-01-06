803511149687a03931183b4fec972bfd5a28fd82
Juliane <juliane.mors@gmail.com>
2018-01-06 13:25:36 +0100

    part 1 quadrillage

--- main.py
+++ main.py
@@ -1,1 +1,92 @@
-﻿print ("Hello")
+﻿##############################
+##     Projet: BOMBOM       ##
+##############################
+import random
+
+import tkinter as tk
+class GameBoard(tk.Frame):
+    def __init__(self, parent, rows=2, columns=2, size=150, color1="green", color2="blue"):
+
+        self.rows = rows
+        self.columns = columns
+        self.size = size
+        self.color1 = color1
+        self.color2 = color2
+        self.pieces = {}
+
+        canvas_width = columns * size
+        canvas_height = rows * size
+
+        tk.Frame.__init__(self, parent)
+        self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0,
+                                width=canvas_width, height=canvas_height, background="bisque")
+        self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
+
+        # this binding will cause a refresh if the user interactively
+        # changes the window size
+        self.canvas.bind("<Configure>", self.refresh)
+
+    def addpiece(self, name, image, row=0, column=0):
+        '''Add a piece to the playing board'''
+        self.canvas.create_image(0,0, image=image, tags=(name, "piece"), anchor="c")
+        self.placepiece(name, row, column)
+
+    def placepiece(self, name, row, column):
+        '''Place a piece at the given row/column'''
+        self.pieces[name] = (row, column)
+        x0 = (column * self.size) + int(self.size/2)
+        y0 = (row * self.size) + int(self.size/2)
+        self.canvas.coords(name, x0, y0)
+
+    def refresh(self, event):
+        '''Redraw the board, possibly in response to window being resized'''
+        xsize = int((event.width-1) / self.columns)
+        ysize = int((event.height-1) / self.rows)
+        self.size = min(xsize, ysize)
+        self.canvas.delete("square")
+        color = self.color2
+        for row in range(self.rows):
+            color = self.color1 if color == self.color2 else self.color2
+            for col in range(self.columns):
+                x1 = (col * self.size)
+                y1 = (row * self.size)
+                x2 = x1 + self.size
+                y2 = y1 + self.size
+                self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=color, tags="square")
+                color = self.color1 if color == self.color2 else self.color2
+        for name in self.pieces:
+            self.placepiece(name, self.pieces[name][0], self.pieces[name][1])
+        self.canvas.tag_raise("piece")
+        self.canvas.tag_lower("square")
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    board = GameBoard(root)
+    board.pack(side="top", fill="both", expand="true", padx=4, pady=4) 
+
+    taillejeu = 2
+    image = []
+
+    for i in range (1,taillejeu+1):
+        image.append(tk.PhotoImage(file="img/"+ str(i)+".gif"))
+
+    for x in range (0,taillejeu):
+        for y in range (0,taillejeu):
+            board.addpiece("piece"+str(x)+str(y), image[y], x,y)
+
+    #board.addpiece("MAIN", image[1],0,0)
+def bouton
+    for i in range ( 1,2)
+        bouton = Button( fenetre, text 'X')
+        bouton.pack
+def tour1
+      if 
+def tour2
+
+def compare 
+    if 
+    list [[1,2][1,2]]
+       
+    root.mainloop()
